4eebb4a8fcea789fb67c5f2d1a41ea1ab757d209
giang <duygiang@vnpt.vn>
2025-07-02 14:06:31 +0700

    thêm api lấy danh sách địa điểm  gần nhất

--- atm-go-api/app/crud/location.py
+++ atm-go-api/app/crud/location.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 from app.models.location import Location
 
 def get_all_locations(db: Session):
@@ -6,3 +7,14 @@ def get_all_locations(db: Session):
 
 def get_banks_by_code_and_type(db: Session, bank_code: str, type: str):
     return db.query(Location).filter(Location.bank_code == bank_code, Location.type == type).all()
+
+def get_nearest_locations(db: Session, latitude: float, longitude: float, limit: int = 5):
+    return (
+        db.query(Location)
+        .order_by(
+            func.pow(Location.latitude - latitude, 2) + func.pow(Location.longitude - longitude, 2)
+        )
+        .limit(limit)
+        .all()
+    )
+	
--- atm-go-api/app/routers/location.py
+++ atm-go-api/app/routers/location.py
@@ -2,26 +2,34 @@ from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 from typing import List, Dict, Union
 from app.dependencies import get_db_connection
-from app.crud.location import get_all_locations
+from app.crud.location import get_all_locations, get_banks_by_code_and_type, get_nearest_locations
 from app.schemas.loacation_schemas import LocationOut  
 
 router = APIRouter()
 
-@router.get("/admin/location", response_model=List[LocationOut])
+@router.get("/location/get-all-location", response_model=List[LocationOut])
 def get_bank_data(db: Session = Depends(get_db_connection)):
     try:
         return get_all_locations(db)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
-@router.get("/admin/location/search", response_model=List[LocationOut])
+@router.get("/location/search", response_model=List[LocationOut])
 def search_banks(
     bank_code: str = Query(..., description="Mã ngân hàng"),
     type: str = Query(..., description="Loại ngân hàng"),
     db: Session = Depends(get_db_connection)
 ):
     try:
-        from app.crud.location import get_banks_by_code_and_type
         return get_banks_by_code_and_type(db, bank_code, type)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+@router.get("/location/get-location-nearest", response_model=List[LocationOut])
+def get_location_nearest(
+    latitude: float = Query(..., description="Vĩ độ người dùng"),
+    longitude: float = Query(..., description="Kinh độ người dùng"),
+    db: Session = Depends(get_db_connection)
+):
+    return get_nearest_locations(db, latitude, longitude)
+	 
